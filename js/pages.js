461e0449212c17d2f139d34ef7c224e96ee15ce9
seonmin <seonmiki@student.42seoul.kr>
2024-08-20 15:07:08 +0900

    Test: 라우팅 연습

---
+++ js/pages.js
@@ -0,0 +1,40 @@
+import MainPage from '../pages/main/main.js'
+import LoginPage from '../pages/login/login.js'
+import MyPage from '../pages/mypage/mypage.js'
+import RankPage from '../pages/rank/rank.js'
+import FriendPage from '../pages/friend/friend.js'
+
+export default container => {
+    const home = () => {
+        const page = new MainPage();
+        container.innerHTML = page.render();
+    }
+
+    const login = () => {
+        const page = new LoginPage();
+        container.innerHTML = page.render();
+    }
+
+    const mypage = () => {
+        const page = new MyPage();
+        container.innerHTML = page.render();
+    }
+
+    const rank = () => {
+        const page = new RankPage();
+        container.innerHTML = page.render();
+    }
+
+    const friend = () => {
+        const page = new FriendPage();
+        container.innerHTML = page.render();
+    }
+  
+    return {
+        home,
+        login,
+        mypage,
+        rank,
+        friend,
+    }
+  }
---
+++ pages/friend/friend.js
@@ -0,0 +1,9 @@
+// MainPage 클래스를 상속하는 새로운 클래스 정의
+export default class FriendPage {
+    // render 메서드를 정의하여 HTML 콘텐츠를 반환
+    render() {
+        return `
+            <h1>Friend Page</h1>
+        `;
+    }
+}
