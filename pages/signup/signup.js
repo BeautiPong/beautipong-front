1dfc13b15282d0394508e6192d87091d21ac82e9
Ingyu Lim <inlim@c1r7s5.42seoul.kr>
2024-08-21 14:31:26 +0900

    Feat: complete signup page

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -5,22 +5,24 @@ export default class SignupPage {
         return `
                 <div class="signup">
                 <div class="signup-container">
-                    <h1>회원 가입</h1>
-                    <form>
-                        <label for="username">로그인에 사용할 아이디를 입력해주세요</label>
-                        <input type="text" id="username" placeholder="아이디" required>
-                        
-                        <label for="password">로그인에 사용할 비밀번호를 입력해주세요</label>
-                        <input type="password" id="password" placeholder="비밀번호" required>
-                        
-                        <label for="email">이메일을 입력해주세요</label>
-                        <input type="email" id="email" placeholder="이메일" required>
-                        
-                        <label for="nickname">닉네임을 입력해주세요</label>
-                        <input type="text" id="nickname" placeholder="닉네임" required>
-                        
-                        <button type="submit">완료</button>
-                    </form>
+                    <div class="signup-contents">
+                        <h1>회원 가입</h1>
+                        <form>
+                            <label for="username">로그인에 사용할 아이디를 입력해주세요</label>
+                            <input type="text" id="username" placeholder="아이디" required>
+
+                            <label for="password">로그인에 사용할 비밀번호를 입력해주세요</label>
+                            <input type="password" id="password" placeholder="비밀번호" required>
+
+                            <label for="email">이메일을 입력해주세요</label>
+                            <input type="email" id="email" placeholder="이메일" required>
+
+                            <label for="nickname">닉네임을 입력해주세요</label>
+                            <input type="text" id="nickname" placeholder="닉네임" required>
+
+                            <button type="submit">완료</button>
+                        </form>
+                    </div>
                 </div>
                 </div>
         `;
