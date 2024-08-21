c41142624ec72e88e0f7df6e936c3bb43fdcd310
choiseoji <choiseoji123@naver.com>
2024-08-21 12:26:31 +0900

    Feat: 로그인 박스 틀

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,21 +3,23 @@ export default class LoginPage {
     // render 메서드를 정의하여 HTML 콘텐츠를 반환
     render() {
         return `
-            <div class="login-container">
-            <h1>PING PONG</h1>
-            <form>
-                <input type="text" placeholder="아이디" required>
-                <input type="password" placeholder="비밀번호" required>
-                <div class="buttons">
-                    <button type="submit">로그인</button>
-                    <button type="button">회원가입</button>
-                </div>
-            </form>
-            <p>or start with
-                <a href="https://42seoul.kr" target="_blank">
-                    <img src="assets/images/42_logo.png" alt="42 Seoul Logo" class="logo">
-                </a>
-            </p>
+            <div class="login">
+                <div class="login-container">
+                    <h1>PING PONG</h1>
+                    <form>
+                        <input type="text" placeholder="아이디" required>
+                        <input type="password" placeholder="비밀번호" required>
+                        <div class="buttons">
+                            <button type="submit">로그인</button>
+                            <button type="button">회원가입</button>
+                        </div>
+                    </form>
+                    <p>or start with
+                        <a href="https://42seoul.kr" target="_blank">
+                            <img src="assets/images/42_logo.png" alt="42 Seoul Logo" class="logo">
+                        </a>
+                    </p>
+                    </div>
             </div>
         `;
     }
