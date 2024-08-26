ce3880ae938f69a926e2acf2a5610b53b8dd09a0
choiseoji <choiseoji123@naver.com>
2024-08-26 13:50:04 +0900

    Feat: 로그인 실패 시 에러 메시지 백에서 받아오기

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,9 +7,11 @@ export default class LoginPage {
                     <form>
                         <div class="text-box">
                             <input type="text" id="userId" placeholder="아이디" name="username" required>
+                            <label for="userID" id="userId_label"></label>
                         </div>
                         <div class="text-box password-box">
                             <input type="text" id="pw" placeholder="패스워드" name="password" required>
+                            <label for="password" id="password_label"></label>
                         </div>
                         <div class="button-box">
                             <button id="signIn-btn" type="button" class="btn">로그인</button>
@@ -66,13 +68,43 @@ export default class LoginPage {
                 window.location.hash = '#/2fa';
             } else {
                 console.error('로그인 실패:', response.status);
+                const errorData = await response.json();
+                this.handleLoginError(errorData);
             }
         } catch (error) {
             console.error('로그인 요청 중 오류 발생:', error);
         }
-
     }
 
+    handleLoginError(errorData) {
+        const userID = document.getElementById('userId');
+        const password = document.getElementById('pw');
+        const userID_label = document.getElementById('userId_label');
+        const password_label = document.getElementById('password_label');
 
+        // 모든 필드의 에러 상태를 초기화
+        userID.classList.remove('form-error');
+        password.classList.remove('form-error');
+        userID_label.classList.remove('from-error-label');
+        password_label.classList.remove('from-error-label');
+
+        // 빈 텍스트로 초기화
+        userID_label.innerText = "";
+        password_label.innerText = "";
+
+        // 에러 메시지에 따른 처리
+        switch (errorData.message) {
+            case "존재하지 않는 아이디입니다." :
+                userID_label.innerText = `${errorData.message}`;
+                userID_label.classList.add("form-error-label");
+                userID.classList.add('form-error');
+                break ;
+            case "비밀번호가 틀렸습니다." :
+                password_label.innerText = `${errorData.message}`;
+                password_label.classList.add("form-error-label");
+                password.classList.add('form-error');
+                break ;
+        }
+    }
 }
 
