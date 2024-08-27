7b047fd53e1502024d7389762ac38cd6c7bdd89a
seonmin <seonmiki@student.42seoul.kr>
2024-08-27 17:19:55 +0900

    Error: 42 oath 로그인 시도

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,5 @@
+import { createModal } from '../../assets/components/modal/modal.js';
+
 export default class LoginPage {
     render() {
         return `
@@ -20,7 +22,7 @@ export default class LoginPage {
                     </form>
                     <div class="start-with">
                         <p>or start with</p>
-                        <img src="../../assets/images/42_logo.svg" alt="42OAuth로그인" class="start-image">
+                        <img src="../../assets/images/42_logo.svg" alt="42OAuth로그인" id="start-with__42logo">
                     </div>
                 </div>
             </div>
@@ -44,8 +46,84 @@ export default class LoginPage {
                 this.handleFormBtn(event);
             }
         });
+
+        // 42 OAuth 로그인 버튼에 클릭 이벤트 리스너 추가
+        const login42Button = document.getElementById('start-with__42logo');
+        login42Button.addEventListener('click', async () => {
+            console.log("42로고 클릭!");
+            try {
+                const response = await fetch('http://localhost:8000/api/user/login/', {
+                    method: 'GET',
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log('42 로그인 요청 성공:', data);
+                    window.location.href = data.redirect_url;
+                } else {
+                    console.error('42 로그인 요청 실패:', response.status);
+                }
+            } catch (error) {
+                console.error('42 로그인 요청 중 오류 발생:', error);
+            }
+        });
+
+        this.handleRedirectCode();
     }
 
+    // 모달 창 생성 및 표시 함수
+    showModal(message, buttonMsg) {
+        // 모달 컴포넌트 불러오기
+        const modalHTML = createModal(message, buttonMsg);
+
+        // 새 div 요소를 생성하여 모달을 페이지에 추가
+        const modalDiv = document.createElement('div');
+        modalDiv.innerHTML = modalHTML;
+        document.body.appendChild(modalDiv);
+
+        // 닫기 버튼에 이벤트 리스너 추가
+        const closeBtn = modalDiv.querySelector('.close');
+        closeBtn.onclick = function() {
+            modalDiv.remove();
+        };
+
+        // 확인 버튼에 이벤트 리스너 추가
+        const confirmBtn = modalDiv.querySelector('.modal-confirm-btn');
+        confirmBtn.onclick = function() {
+            modalDiv.remove();
+        };
+    }
+
+    async handleRedirectCode() {
+        // URL에서 'code' 파라미터 추출
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        console.log("코드 테스트");
+
+        if (code) {
+            // 'code'가 존재하는 경우, 서버로 POST 요청을 보내어 액세스 토큰을 요청합니다.
+            try {
+                const response = await fetch('http://localhost:8000/api/user/get-token/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ code }),  // 'code'를 서버에 전달
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log('42 회원가입 성공:', data);
+                    window.location.href = '#/login'; // 로그인 페이지로 이동
+                    this.showModal('회원가입이 성공적으로 완료되었습니다!', '확인');
+                } else {
+                    console.error('42 토큰 요청 실패:', response.status);
+                }
+            } catch (error) {
+                console.error('42 토큰 요청 중 오류 발생:', error);
+            }
+        }
+    }
 
 	async handleFormBtn(event) {
         event.preventDefault(); // 기본 폼 제출 이벤트를 막습니다.
