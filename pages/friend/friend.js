26fc2843b57143915f79990373064726c6a7ee49
choiseoji <choiseoji123@naver.com>
2024-08-27 16:47:05 +0900

    Feat: 친구 페이지 js 추가

--- pages/friend/friend.js
+++ pages/friend/friend.js
@@ -3,7 +3,32 @@ export default class FriendPage {
     // render 메서드를 정의하여 HTML 콘텐츠를 반환
     render() {
         return `
-            <h1>Friend Page</h1>
+            <div class="friend">
+                <div class="friend-container">
+                    <div class="friend-list-container">
+                        <div class="friend-request">
+                            <div class="friend-request-search">
+                                <p class="friend-request-text">친구요청</p>
+                                <img class="friend-search-icon" src="../../assets/icons/userSearch.svg" alt="친구찾기">
+                            </div>
+                            <div class="friend-request-box">
+                                <p>새로운 친구요청이 없습니다</p>
+                            </div>
+                        </div>
+                        <div class="friend-list">
+                            <p class="friend-list-text">친구목록</p>
+                            <div class="friend-list-box">
+                                <p>친구가 없습니다..</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat-container">
+                        <div class="chat-box">
+                            <p class="chat-box-text">친구에게 메시지를 보내보세요!!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         `;
     }
 }
