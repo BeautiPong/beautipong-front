6114ca5c9ae6f3aaf731f4cd37a75f2c1d6033be
seonmiki <seonmiki@student.42seoul.kr>
2024-09-27 04:36:48 +0900

    Feat: 마이페이지 닉네임 변경

--- assets/components/modal/modal.js
+++ assets/components/modal/modal.js
@@ -1,6 +1,5 @@
 // Modal 컴포넌트 함수
 export function createModal(message, buttonMsg, icon) {
-    console.log(icon);
     return `
         <div class="modal">
             <div class="modal-box">
@@ -8,6 +7,21 @@ export function createModal(message, buttonMsg, icon) {
                 <img src="${icon ? `../../assets/icons/${icon}.svg` : '../../assets/icons/invite.svg'}" alt="아이콘" class="signup-image">
                 <p class="message">${message}</p>
                 <button class="modal-confirm-btn">${buttonMsg}</button>
+            </div>
+        </div>
+    `;
+}
+
+export function createNicknameModal(message, buttonMsg) {
+    return `
+        <div class="modal">
+            <div class="modal-box">
+                <span class="close">&times;</span>
+                <p class="message">${message}</p>
+                <div id="new_nickname_inputbox">
+                    <input type="text" id="new_nickname" name="nickname" required>
+                    <button class="modal-confirm-btn" id="nickname-change-btn">${buttonMsg}</button>
+                </div>
             </div>
         </div>
     `;
