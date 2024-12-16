df67c3030635c45eddde235bb208d58f25296b84
choiseoji <choiseoji123@naver.com>
2024-12-16 10:56:56 +0900

    Feat: refuse api -> delete api 로 수정

--- pages/friend/friend.js
+++ pages/friend/friend.js
@@ -185,7 +185,7 @@ export default class FriendPage {
         if (refuseButton) {
             refuseButton.addEventListener('click', async function () {
                 try {
-                    const response = await fetch(`https://${SERVER_IP}/api/friend/refuse/${sender}/`, {
+                    const response = await fetch(`https://${SERVER_IP}/api/friend/delete/${sender}/`, {
                         method: 'POST',
                         headers: {
                             'Authorization': `Bearer ${token}`
